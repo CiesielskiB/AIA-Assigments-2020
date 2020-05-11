bccad69601abb1959453a7057a9d75690cd3b0ec
BartoszCie <Miketen1997@gmail.com>
2020-05-11 21:25:38 +0200

    Excercise 6 Microsoft tutorial

---
+++ Excercise 6 - Web Pages/Excercise6WebPages/Data/Excercise6WebPagesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Excercise6WebPages.Models;
+
+namespace Excercise6WebPages.Data
+{
+    public class Excercise6WebPagesContext : DbContext
+    {
+        public Excercise6WebPagesContext (DbContextOptions<Excercise6WebPagesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Excercise6WebPages.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Excercise 6 - Web Pages/Excercise6WebPages/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Excercise6WebPages.Data;
+using Excercise6WebPages.Models;
+
+namespace Excercise6WebPages.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Excercise6WebPages.Data.Excercise6WebPagesContext _context;
+
+        public DetailsModel(Excercise6WebPages.Data.Excercise6WebPagesContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
