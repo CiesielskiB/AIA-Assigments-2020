072e5110adce7e87393fc598bf382dc2663ad81f
BartoszCie <Miketen1997@gmail.com>
2020-05-29 14:32:38 +0200

    Excercise 7 - mvc

---
+++ Excercise 7 - MVC/MoviesTutorial/Data/MoviesTutorialContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesTutorial.Models;
+
+namespace MoviesTutorial.Data
+{
+	public class MoviesTutorialContext : DbContext
+	{
+		public MoviesTutorialContext(DbContextOptions<MoviesTutorialContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Movie> Movie { get; set; }
+	}
+}
